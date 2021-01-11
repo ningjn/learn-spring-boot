4446598a72f174d62af36a5520f4317f46c8feea
ningth <ningth@inspur.com>
2021-01-11 21:07:53 +0800

    init

---
+++ gs-rest-service/src/test/java/org/lambda/gsrestservice/GsRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lambda.gsrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
