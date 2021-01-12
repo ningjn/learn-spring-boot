19fd36ec3fde9ea4350d600e402d991abd47346e
ningth <ningth@inspur.com>
2021-01-12 14:50:47 +0800

    102-gs-consuming-rest

---
+++ gs-consuming-rest/src/main/java/org/lambda/gsconsumingrest/GsConsumingRestApplication.java
@@ -0,0 +1,12 @@
+package org.lambda.gsconsumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsConsumingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsConsumingRestApplication.class, args);
+	}
+}
---
+++ gs-consuming-rest/src/test/java/org/lambda/gsconsumingrest/GsConsumingRestApplicationTests.java
@@ -0,0 +1,11 @@
+package org.lambda.gsconsumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsConsumingRestApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
