8b3da2ab2a75873e744e11f03f11127ea2859072
ningth <ningth@inspur.com>
2021-01-12 16:40:13 +0800

    103-gs-accessing-data-jpa

--- gs-accessing-data-jpa/src/test/java/org/lambda/gsaccessingdatajpa/ApplicationTests.java
+++ gs-accessing-data-jpa/src/test/java/org/lambda/gsaccessingdatajpa/ApplicationTests.java
@@ -1,11 +1,33 @@
 package org.lambda.gsaccessingdatajpa;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
 class ApplicationTests {
 
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private CustomerRepository customers;
+
 	@Test
-	void contextLoads() {}
+	public void testFindByLastName() {
+		Customer customer = new Customer("first", "last");
+		entityManager.persist(customer);
+
+		List<Customer> findByLastName = customers.findByLastName(
+			customer.getLastName()
+		);
+
+		assertThat(findByLastName)
+			.extracting(Customer::getLastName)
+			.containsOnly(customer.getLastName());
+	}
 }
