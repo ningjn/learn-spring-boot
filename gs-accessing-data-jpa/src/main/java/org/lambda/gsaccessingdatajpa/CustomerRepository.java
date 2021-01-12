9baa1f49298d806d5ea2beaa37a4f147c4cea15d
ningth <ningth@inspur.com>
2021-01-12 16:35:04 +0800

    103-gs-accessing-data-jpa

--- gs-accessing-data-jpa/src/main/java/org/lambda/gsaccessingdatajpa/CustomerRepository.java
+++ gs-accessing-data-jpa/src/main/java/org/lambda/gsaccessingdatajpa/CustomerRepository.java
@@ -9,5 +9,10 @@ import org.springframework.data.repository.CrudRepository;
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 	List<Customer> findByLastName(String lastName);
 
+	/**
+	 * 根据id获取Customer
+	 * @param id id
+	 * @return customer对象
+	 */
 	Customer findById(long id);
 }
