95165acd79603ef64d9d69c5e83ef6a83c73c50c
ningth <ningth@inspur.com>
2021-01-12 09:28:52 +0800

    gs-rest-service

--- gs-rest-service/src/main/java/org/lambda/gsrestservice/GreetingController.java
+++ gs-rest-service/src/main/java/org/lambda/gsrestservice/GreetingController.java
@@ -1,4 +1,24 @@
 package org.lambda.gsrestservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Administrator
+ */
+@RestController
 public class GreetingController {
+    public static final String template = "hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "World")
+                    String name
+    ) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
