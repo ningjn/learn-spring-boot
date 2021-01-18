db181dddfac619e89a4c8a18071e57b89cafd413
ningth <ningth@inspur.com>
2021-01-18 20:54:45 +0800

    [feat]add swagger

--- gs-rest-service/src/main/java/org/lambda/gsrestservice/GreetingController.java
+++ gs-rest-service/src/main/java/org/lambda/gsrestservice/GreetingController.java
@@ -1,5 +1,8 @@
 package org.lambda.gsrestservice;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,15 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Administrator
  */
+@Api(tags = "用户管理")
 @RestController
 public class GreetingController {
 
 	public static final String TEMPLATE = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@ApiOperation("问候")
 	@GetMapping("/greeting")
 	public Greeting greeting(
-		@RequestParam(value = "name", defaultValue = "World") String name
+		@ApiParam("名称") @RequestParam(
+			value = "name",
+			defaultValue = "World"
+		) String name
 	) {
 		return new Greeting(
 			counter.incrementAndGet(),
--- gs-rest-service/src/main/java/org/lambda/gsrestservice/GsRestServiceApplication.java
+++ gs-rest-service/src/main/java/org/lambda/gsrestservice/GsRestServiceApplication.java
@@ -2,10 +2,12 @@ package org.lambda.gsrestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author Administrator
  */
+@EnableOpenApi
 @SpringBootApplication
 public class GsRestServiceApplication {
 
